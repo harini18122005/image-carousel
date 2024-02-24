0383a4660112bb22a81451f01b840e7f69e4cb78
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2024-02-24 22:12:44 +0530

    first commit

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./Carousel.css";
+import { images } from "../data/CarouselData";
+// you can explore more - and check as how to use materiul ui
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+// implement the class below
+class Carousel extends Component {
+constructor(){
+    super()
+    this.state={
+        count:0
+    }
+}
+handleClick=(operation)=>{
+    operation='left'? this.state.count!==0 ?  this.setState({count:this.state.count-1}) :   this.setState({count:images.length-1}) : this.state.count!=images.length-1 ? this.setState({count:this.state.count+1}) :    this.setState({count:0})
+
+}
+
+  render() {
+    return (
+      <div className="carousel">
+        <div
+          className="innercarousel"
+          style={{ backgroundImage: `url(${images[this.state.count].img})` }}>
+
+          <div className="leftbutton" onClick={()=>this.handleClick("left")}>
+            <ArrowBackIosIcon style={{ color: "white", fontSize: "30px" }} />
+          </div>
+
+          <div id="centerText">
+            <h1>{images[this.state.count].title}</h1>
+            <p>{images[this.state.count].subtitle}</p>
+          </div>
+
+          <div className="rightbutton" onClick={()=>this.handleClick("right")}>
+            <ArrowForwardIosIcon style={{ color: "white", fontSize: "30px" }} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
